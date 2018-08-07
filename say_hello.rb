78a3b3fbd51abfa8df26b9bed7107cc05efea347
firyalmahakhan <firyalmahak@gmail.com>
2018-08-07 16:14:11 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,11 +2,11 @@ def say_hello(your_name = "Ruby Programmer")
   puts "Hello #{your_name}!"
 end
 say_hello
-# say_hello("Maha")
-# say_hello("Hannah")
-# say_hello("Janet")
+say_hello("Maha")
+say_hello("Hannah")
+say_hello("Janet")
 
-# def say_hello(your_name, my_name)
-#   puts "Hello, #{your_name}! I'm #{my_name}."
-# end
-# say_hello("Claire","Allison")
+def say_hello(your_name, my_name)
+  puts "Hello, #{your_name}! I'm #{my_name}."
+end
+say_hello("Claire","Allison")
